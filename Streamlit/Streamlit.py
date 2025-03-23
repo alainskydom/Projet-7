80ab66c446e6e5e07e40e6d5257ea42411f6c2e6
alainskydom <alain.scialom@gmail.com>
2025-03-23 21:42:07 +0100

    Update Streamlit.py

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -36,7 +36,7 @@ best_model = load_model()
 
 
 # Charger les données
-df_ = pd.read_csv(r"(r"df_api.csv")
+df_ = pd.read_csv(r"df_api.csv")
 df_=df_.loc[:, ~df_.columns.str.match ('Unnamed')]
 df_calc= df_.drop(['TARGET', 'SK_ID_CURR'], axis=1)
 # df.drop(columns='index', inplace=True)
