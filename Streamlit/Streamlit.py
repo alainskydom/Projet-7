b20388e77eba589be96a4017bebe62f339df9f29
alainskydom <alain.scialom@gmail.com>
2025-03-24 22:09:29 +0100

    Update Streamlit.py

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -91,17 +91,17 @@ if run:
 
     if score >= threshold :
         st.balloons()
-        t1 = plt.Polygon([[5, 0.5], [5.5, 0], [4.5, 0]], color='black')
+        t1 = plt.Polygon([[5, 0.5], [5.5, 0], [4.5, 0]], color='red')
         placeholder.markdown('Your credit score is **GOOD**! Congratulations!')
         st.markdown(
             'This credit score indicates that this person is likely to repay a loan, so the risk of giving them credit is low.')
     elif score < threshold:
-        t1 = plt.Polygon([[3, 0.5], [3.5, 0], [2.5, 0]], color='black')
+        t1 = plt.Polygon([[3, 0.5], [3.5, 0], [2.5, 0]], color='red')
         placeholder.markdown('Your credit score is **REFUSED**.')
         st.markdown(
             'This credit score indicates that this person is likely to repay a loan, but can occasionally miss some payments. Meaning that the risk of giving them credit is medium.')
     elif score == -1:
-        t1 = plt.Polygon([[1, 0.5], [1.5, 0], [0.5, 0]], color='black')
+        t1 = plt.Polygon([[1, 0.5], [1.5, 0], [0.5, 0]], color='red')
         placeholder.markdown('Your credit score is **POOR**.')
         st.markdown(
             'This credit score indicates that this person is unlikely to repay a loan, so the risk of lending them credit is high.')
