0d201ecd89044ceeb1eeb96b48aa26afa846297a
alainskydom <alain.scialom@gmail.com>
2025-04-01 20:57:14 +0200

    Update Streamlit.py

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -73,6 +73,7 @@ if df_selection.empty:
 
 st.sidebar.write("**Age du client :**", int(df_.iloc[id,2]/365), "ans")
 st.sidebar.write("**Durée du crédit :**", int(df_.iloc[id,0]), "ans")
+st.sidebar.write("**Ancienneté dans l'emploi :**", int(df_.iloc[id,4]/365), "ans")
 
 run = st.button( 'Evaluer le score de crédit de la demande')
 
