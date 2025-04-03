8b3b308737819f880e047e835bf46ce394da0d95
alainskydom <alain.scialom@gmail.com>
2025-04-03 16:12:35 +0200

    Update Streamlit.py

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -142,7 +142,7 @@ for feature in features:
         if feature=='DAYS_BIRTH':
         # Plot the distribution of feature
           st.write( feature)
-          age=df_['DAYS_BIRTH']/365
+          age=df_['DAYS_BIRTH']/365.values
           h1=plt.hist(age.astype(int), edgecolor = 'k', bins = 25)
           plt.axvline(int(df_[feature][df_.index==id]), color="red", linestyle=":")
           plt.title(feature + " distribution", size=5)
