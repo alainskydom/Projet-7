3204b5f0d5841a1834985664ca3b63fbe3237776
alainskydom <alain.scialom@gmail.com>
2025-04-03 16:31:47 +0200

    Update Streamlit.py

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -156,7 +156,7 @@ for feature in features:
           st.write( feature)
           Anciennete=df_['DAYS_EMPLOYED']/-365
           h1=plt.hist(Anciennete, edgecolor = 'k', bins = 25)
-          plt.axvline(int(df_[feature][df_.index==id]/365), color="red", linestyle=":")
+          plt.axvline(int(df_[feature][df_.index==id]/-365), color="red", linestyle=":")
           plt.title(feature + " distribution", size=5)
           plt.xlabel(feature, size=5)
           plt.ylabel('Fréquence', size=5)
